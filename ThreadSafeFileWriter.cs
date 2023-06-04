89604de7de4342b5df1b10664fd57297548efb33
mastermeric <mastermeric@gmail.com>
2023-06-04 23:37:31 +0300

    Mutex log writer..

--- ThreadSafeFileWriter.cs
+++ ThreadSafeFileWriter.cs
@@ -1,7 +1,29 @@
 namespace TCPServer
 {
+
+    //The Mutex will let threads wait for the mutex handle in a queue until the mutex is released. 
+    //So if 3 threads will write to the same file, they will wait in line nicely until they are granted access.
+
+    //It is very important that the code releases the mutex as well, or the code will be locked forever.
+    //Mutexes can be named, like in the example above. This locks the shared resource system wide. 
+    //So if another process tries to access the same code, that process will also wait in line. 
+    //Backslashes in mutex names are a reserved character and must be removed.
+
+
+
     public class ThreadSafeFileWriter
     {
+        readonly string mutexReaderName = DateTime.Now.ToShortDateString().Replace(" ", "")
+        .Replace(".", "").Replace(":", "").Replace("/", "")
+        .Replace("\\", "").Replace("_", "").Replace("-", "")
+        + "_ReaderMutex";
+
+        readonly string mutexWriterName = DateTime.Now.ToShortDateString().Replace(" ", "")
+        .Replace(".", "").Replace(":", "").Replace("/", "")
+        .Replace("\\", "").Replace("_", "").Replace("-", "")
+        +"_WriterrMutex";
+
+
         readonly string dailyLogFile = DateTime.Now.ToShortDateString().Replace(" ", "")
         .Replace(".", "").Replace(":", "").Replace("/", "")
         .Replace("\\", "").Replace("_", "").Replace("-", "")
@@ -16,7 +38,7 @@ namespace TCPServer
 
         public void WriteLogFile(string fileContents)
         {
-            using (var mutex = new Mutex(false, dailyLogFile))
+            using (var mutex = new Mutex(false))
             {
                 var hasHandle = false;
                 try
@@ -49,7 +71,7 @@ namespace TCPServer
         public string ReadLogFile()
         {
             // This block will be protected area
-            using (var mutex = new Mutex(false, dailyLogFile))
+            using (var mutex = new Mutex(false))
             {
                 var hasHandle = false;
                 try
@@ -57,7 +79,7 @@ namespace TCPServer
                     // Wait for the muted to be available
                     hasHandle = mutex.WaitOne(Timeout.Infinite, false);
                     // Do the file read
-                    if (!File.Exists(dailyLogFile)) 
+                    if (!File.Exists(dailyLogFile))
                     {
                         return string.Empty;
                     }
@@ -75,35 +97,6 @@ namespace TCPServer
                     {
                         mutex.ReleaseMutex();
                     }
-                }
-            }
-        }
-
-        public string ReadErrorFile(string filePathAndName)
-        {
-            // This block will be protected area
-            using (var mutex = new Mutex(false, filePathAndName.Replace("\\", "")))
-            {
-                var hasHandle = false;
-                try
-                {
-                // Wait for the muted to be available
-                hasHandle = mutex.WaitOne(Timeout.Infinite, false);
-                // Do the file read
-                if (!File.Exists(filePathAndName))
-                    return string.Empty;
-                return File.ReadAllText(filePathAndName);
-                }
-                catch (Exception)
-                {
-                throw;
-                }
-                finally
-                {
-                // Very important! Release the mutex
-                // Or the code will be locked forever
-                if (hasHandle)
-                    mutex.ReleaseMutex();
                 }
             }
         }
